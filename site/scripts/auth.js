11f7176d3d272383894ed5dcd709d6e0ddc357c5
Zavien Sibilia <zaviensibilia@hotmail.com>
2023-10-17 23:06:05 -0700

    eureka

--- site/scripts/auth.js
+++ site/scripts/auth.js
@@ -11,7 +11,7 @@ const app = new msal.PublicClientApplication(msalParams);
 async function getToken() {
   let accessToken = "";
 
-  const authParams = { scopes: ["Files.ReadWrite.All", "User.Read"] };
+  const authParams = { scopes: ["OneDrive.ReadWrite"] };
 
   try {
     // see if we have already the idtoken saved
