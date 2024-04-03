918d5eb85f0ace0bee91f44acb6f9fec5162032f
Alvin Alaphat <33328305+alvinalaphat@users.noreply.github.com>
2024-04-03 11:58:16 -0700

    Update auth.js

--- site/scripts/auth.js
+++ site/scripts/auth.js
@@ -11,7 +11,7 @@ const app = new msal.PublicClientApplication(msalParams);
 async function getToken() {
   let accessToken = "";
 
-  const authParams = { scopes: ["OneDrive.ReadWrite"] };
+  const authParams = { scopes: ["OneDrive.Read"] };
 
   try {
     // see if we have already the idtoken saved
